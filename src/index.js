7bbff83cb208056f98b1219e272507b9745a228a
Prathmeshdeshmukh <87267186+Prathmeshdeshmukh@users.noreply.github.com>
2023-03-30 20:03:55 +0530

    created notification model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const {PORT} = require('./config/serverConfig')
-
+const cron =require('cron')
 const { sendBasicEmail } = require('./services/email-service')
 const setupAndStartServer =() =>{
     const app = express();
@@ -9,12 +9,22 @@ const setupAndStartServer =() =>{
     app.use(bodyParser.urlencoded({extended: true}))
     app.listen(PORT , ()=>{
         console.log('server started on PORT', PORT);
-        sendBasicEmail(
-            "support@admin.com",
-            "prathmeshd60@gmail.com",
-            "TestingMail",
-            "Hey hope you like our service"
-            )
+        
+        var CronJob = require('cron').CronJob;
+        var job = new CronJob(
+            '* * * * * *',
+            function() {
+                // sendBasicEmail(
+                //     "support@admin.com",
+                //     "prathmeshd60@gmail.com",
+                //     "TestingMail",
+                //     "Hey hope you like our service"
+                //     )
+            },
+            null,
+            true,
+            'America/Los_Angeles'
+        );
     })
 }
 
