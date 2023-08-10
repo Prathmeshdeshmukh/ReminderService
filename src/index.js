e9cbda34ec5a3bd0713aedb65218fc3a039cf2a1
Prathmeshdeshmukh <prathmeshd60@gmail.com>
2023-08-10 10:31:44 +0530

    completed msg Queue setup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const {PORT} = require('./config/serverConfig')
-const cron =require('cron')
-const { sendBasicEmail } = require('./services/email-service')
+const {PORT, REMINDER_BINDING_KEY} = require('./config/serverConfig')
+const cron = require('cron')
+// const { sendBasicEmail } = require('./services/email-service')
 const jobs = require('./utils/jobs');
 const TicketController = require('./controllers/ticket-controller');
+const { subscribeMessage, createChannel } = require('./utils/messageQueues');
+const EmailService = require('./services/email-service');
 
-const setupAndStartServer =() =>{
+
+const setupAndStartServer = async() =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
 
     app.post('/api/v1/tickets', TicketController.create)
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);  
+
     app.listen(PORT , ()=>{
         console.log('server started on PORT', PORT);
-       
         jobs();
     })
 }
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -43,9 +43,26 @@ const updateticket = async (ticketId, data)=>{
     }
 }
 
+const subscribeEvents = async(payload) =>{
+    let service = payload.service;
+    let data = payload.data;
+    switch(service){
+        case 'SEND_BASIC_MAIL':
+            await sendBasicEmail(data);
+            break;
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        default : 
+            console.log('invalid service');
+            break;
+    }
+}
+
 module.exports = {
     sendBasicEmail,
     fetchPendingEmails,
     createNotification,
-    updateticket
+    updateticket,
+    subscribeEvents
 }
